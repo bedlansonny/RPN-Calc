302b805db9f6bbe524a475a1dace117cf00b7890
bedlansonny <bedlansonny@gmail.com>
2017-08-26 13:08:23 -0500

    Operator Interpretation

--- src/RPNCalc.java
+++ src/RPNCalc.java
@@ -4,9 +4,10 @@ public class RPNCalc
 {
     public static void main(String args[]) throws FileNotFoundException
     {
-        File file = new File("H:\\CS\\RPNCalc\\src\\input.txt");
+        File file = new File(System.getProperty("user.dir") + "\\src\\input.txt");
         Scanner sc = new Scanner(file);
 
+        //I chose 10 randomly. The most obvious flaw in this mini project is not changing size.
         ArrayStack stack = new ArrayStack(10);
 
         while(sc.hasNextDouble())
@@ -16,8 +17,36 @@ public class RPNCalc
 
         while(sc.hasNext())
         {
-            String operator = sc.next();
-            //tbc (to be continued...)
+            String oper = sc.next();
+            double b = stack.pop();
+            double a = stack.pop();
+
+            if(oper.equals("+"))
+            {
+                stack.push(a + b);
+            }
+            else if(oper.equals("-"))
+            {
+                stack.push(a - b);
+            }
+            else if(oper.equals("*"))
+            {
+                stack.push(a * b);
+            }
+            else if(oper.equals("/"))
+            {
+                stack.push(a / b);
+            }
+        }
+
+        if(stack.size() != 1)
+        {
+            System.out.println("ERROR: improper amount of numbers and operators.");
+            System.out.println(stack);
+        }
+        else
+        {
+            System.out.println(stack.peek());
         }
 
     }
